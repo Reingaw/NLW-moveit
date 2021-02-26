abe8c826f7ca8b7cc55b1fcb6c4935ee76a22f51
Wagner Andrade <wagnerlinhares@bol.com.br>
2021-02-26 20:35:39 -0300

    Finished the Fourth Day

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css';
 import { ChallengeBox } from "../components/ChallengeBox";
+import { CountdownProvider } from "../contexts/CountdownContext";
 
 export default function Home() {
   return (
@@ -14,19 +15,22 @@ export default function Home() {
       <Head>
         <title>Inicio | move.it</title>
       </Head>
+
       <ExperienceBar />
 
-      <section>
-        <div>
-          <Profile />
-          <CompletedChallenges />
-          <Countdown />
-        </div>
+      <CountdownProvider>
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <Countdown />
+          </div>
 
-        <div>
-          <ChallengeBox />
-        </div>
-      </section>
+          <div>
+            <ChallengeBox />
+          </div>
+        </section>
+      </CountdownProvider>
     </div>  
   )
 }
